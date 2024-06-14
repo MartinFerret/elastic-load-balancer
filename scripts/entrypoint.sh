161154d3bc58937e7d8b89e3d9a3ccbb3e8620cc
Martin Ferret <firdaouszm@gmail.com>
2024-06-14 10:26:42 +0200

    fully working

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+envsubst '\$PORT' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html.tmp
+mv /usr/share/nginx/html/index.html.tmp /usr/share/nginx/html/index.html
+exec "$@"
